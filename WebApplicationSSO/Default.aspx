223728628874fa8e698b041178ee7165d96c5485
akasharvindmane <akasharvindmane@gmail.com>
2020-03-02 15:21:11 +0800

    second commit

---
+++ WebApplicationSSO/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplicationSSO.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <span style="float:right">
+                <asp:button ID="Timer1" runat="server" Interval="1000" OnClick="lnkbtnlogout_Click" Text="Logout">
+                </asp:button>
+            </span>
+            <h2>Axiata Azure AD User</h2>
+            <h1><asp:label ID="signedIn" runat="server" /></h1> <hr /> 
+            <asp:label ID="claimType" runat="server" />  <hr /> 
+            <asp:label ID="claimValue" runat="server" />  <hr /> 
+            <asp:label ID="claimValueType" runat="server" />  <hr /> 
+            <asp:label ID="claimSubjectName" runat="server" />  <hr /> 
+            <asp:label ID="claimIssuer" runat="server" />  <hr /> 
+             <%--<asp:label ID="originalIssuer" runat="server" />  <hr />--%> 
+             <%--<asp:label ID="properties" runat="server" />  <hr />--%> 
+        </div>
+        <div>  
+            <h2>Axiata Database User</h2>
+                <asp:GridView ID="GridView1" runat="server" BackColor="#FFF" BorderColor="#000" BorderStyle="None" BorderWidth="1px" CellPadding="3" CellSpacing="2">  
+                   <FooterStyle BackColor="White" ForeColor="Black" />  
+                    <HeaderStyle BackColor="White" Font-Bold="True" ForeColor="Black" />  
+                    <PagerStyle ForeColor="Black" HorizontalAlign="Center" />  
+                    <RowStyle BackColor="White" ForeColor="Black" />  
+                    <SelectedRowStyle BackColor="#fff" Font-Bold="True" ForeColor="black" />  
+                    <SortedAscendingCellStyle BackColor="#fff" />  
+                    <SortedAscendingHeaderStyle BackColor="#fff" />  
+                    <SortedDescendingCellStyle BackColor="#fff" />  
+                    <SortedDescendingHeaderStyle BackColor="#fff" /></asp:GridView>  
+            </div> 
+        
+            
+    </form>
+</body>
+</html>
---
+++ WebApplicationSSO/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.IdentityModel.Claims;
+using System.Threading;
+using System.Web;
+using System.Linq;
+using System.ComponentModel;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web.Security;
+
+namespace WebApplicationSSO
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+                    var identity = User.Identity as ClaimsIdentity;
+                    ClaimsPrincipal claimsPrincipal = Thread.CurrentPrincipal as ClaimsPrincipal;
+            if (claimsPrincipal.Identity.IsAuthenticated)
+            {
+                //Created a instance of contextDB.  
+                using (axiatademodbEntities1 context = new axiatademodbEntities1())
+                {
+                    var userDetails = context.users.Where(x => x.email == User.Identity.Name).Select(x => new { x.email, x.role, x.username }).ToList();
+                    if (userDetails != null)
+                    {
+                        IList listSource = userDetails;
+
+                        GridView1.DataSource = listSource;
+                        GridView1.DataBind();
+                    }
+
+                }
+
+                HttpContext.Current.User = claimsPrincipal;
+                System.Threading.Thread.CurrentPrincipal = System.Web.HttpContext.Current.User;
+
+                
+                    foreach (Claim claim in identity.Claims)
+                    {
+                        claimType.Text = claim.ClaimType;
+                        claimValue.Text = claim.Value;
+                        claimValueType.Text = claim.ValueType;
+                        claimSubjectName.Text = claim.Subject.Name;
+                        claimIssuer.Text = claim.Issuer;
+                        //originalIssuer.Text = claim.OriginalIssuer;
+                        //properties.Text = claim.Properties.ToString();
+                    }
+
+            }
+            else
+            {
+                Microsoft.IdentityModel.Web.FederatedAuthentication.SessionAuthenticationModule.SignOut();
+                Microsoft.IdentityModel.Web.FederatedAuthentication.SessionAuthenticationModule.DeleteSessionTokenCookie();
+                Microsoft.IdentityModel.Web.FederatedAuthentication.SessionAuthenticationModule.SignOut();
+                Response.Redirect("~/AccessDenied.aspx");
+            }
+          
+        }
+        protected void lnkbtnlogout_Click(object sender, EventArgs e)
+        {
+            Microsoft.IdentityModel.Web.FederatedAuthentication.SessionAuthenticationModule.SignOut();
+            Microsoft.IdentityModel.Web.FederatedAuthentication.SessionAuthenticationModule.DeleteSessionTokenCookie();
+            Microsoft.IdentityModel.Web.FederatedAuthentication.SessionAuthenticationModule.SignOut();
+
+            Response.Redirect("~/Logout.aspx");
+        }
+    }
+}
